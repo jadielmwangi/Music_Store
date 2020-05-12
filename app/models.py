18531a17374aa4204b033f039f952d030cccd11c
derrickip34 <derrickip34@gmail.com>
2020-05-12 16:57:13 +0300

    edit User models class

--- app/models.py
+++ app/models.py
@@ -13,12 +13,10 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
 
     pass_secure = db.Column(db.String(255))
-    reviews = db.relationship('Review',backref='user',lazy="dynamic")
 
     @property
     def password(self):
