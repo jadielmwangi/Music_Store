40be0a1d71fea7c716bb197060cefb48c78e05d6
derrickip34 <derrickip34@gmail.com>
2020-05-12 15:23:05 +0300

    create configuration classes

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config():
+
+    SECRET_KEY = 'derokip34'
+
+
+class ProdConfig(Config):
+
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
