4ff2ddeba447018eff2ca7b0199cd72b1fc778c4
derrickip34 <derrickip34@gmail.com>
2020-05-12 15:23:51 +0300

    register auth_blueprint

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask_login import LoginManager
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from config import config_options
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    return app
